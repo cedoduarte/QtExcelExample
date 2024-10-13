7ae9889d2abc61dd275eb00ebb2ae8f8b20c9fc9
Carlos Duarte <carlosduarte.1@hotmail.com>
2024-10-12 19:48:14 -0700

    delete compilation folder

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_QtExcelExample_FILE C:/Users/carlo/OneDrive/Documents/QtProjects/QtExcelExample/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/QtExcelExample.exe)
-set(__QT_DEPLOY_TARGET_QtExcelExample_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_QtExcelExample_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
